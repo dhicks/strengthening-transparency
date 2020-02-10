4f7ad45efc6b51d18023cbd707822e74b55a22e8
Dan Hicks <hicks.daniel.j@gmail.com>
2020-02-10 11:01:06 -0800

    1. pass at extract text

---
+++ scripts/02_prep_text.R
@@ -0,0 +1,49 @@
+## This script wrangles the text of the attachments
+library(tidyverse)
+library(pdftools)
+library(officer)
+
+library(furrr)
+plan(multisession, workers = 3)
+
+library(tictoc)
+library(assertthat)
+
+source('../R/extract_text.R')
+
+prefix = '02_'
+data_folder = '../data/'
+# attachment_folder = str_c(data_folder, '01_attachments/')
+attachment_text_folder = str_c(data_folder, prefix, 'attachment_text/')
+
+## Load data ----
+attachments_df = read_rds(str_c(data_folder, '01_attachments.Rds')) %>% 
+    rename(dl_path = path) %>% 
+    mutate(text_path = str_c(attachment_text_folder, filename, '.txt'))
+
+
+## Extract text ----
+## ~137 sec if everything needs to be extracted
+tic()
+attach_text = attachments_df %>% 
+    # head(100) %>%
+    mutate(text = future_pmap(list(dl_path, text_path, ext), 
+                              extract_text, force = FALSE, 
+                              .progress = TRUE), 
+           text = map_chr(text, str_flatten, collapse = '\n'), 
+           text = str_squish(text)) %>% 
+    select(-matches('path'))
+toc()
+
+
+## Some EDA/error checking ----
+## Very short documents -> some scanned docs need OCR
+## Two remaining:  
+## one that's totally blank; one that's a handwritten postcard
+attach_text %>%
+    mutate(len = str_length(text)) %>%
+    filter(len < 100) %>%
+    select(filename, ext, len)
+
+
+## TODO:  more error checking; write output; Makefile
